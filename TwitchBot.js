2106f6db8c5e26cf552e5e440ccf2a740b3e2194
EphraimRuttenberg <ephraim408@gmail.com>
2020-07-24 22:40:50 -0400

    Fixed MarkovChain.selectChunk() and makeChain()

--- TwitchBot.js
+++ TwitchBot.js
@@ -1,23 +1,21 @@
 const MarkovChain = require("./MarkovChain.js");
 const tmi = require("tmi.js");
 const fs = require("fs");
-const stream = require("./StreamStats.js");
 
 var textData = {};
 var allChunks = {};
 
 const config = JSON.parse(fs.readFileSync("credentials.json", "utf8"));
 const blacklist = JSON.parse(fs.readFileSync("blacklist.json", "utf8"));
-const commandCd = 45000;
-const resetCd = 1200000;
-const streamCheckCd = 10000;
-const ignoredNames = ["nacholistic", "nightbot", "scootycoolguy"]
+const commandCd = 0;//10000;
+const resetCd = 1800000;
+const ignoredNames = ["nightbot", "scootycoolguy"]
 const channel = config.channels[0];
 const clientid = config.clientid;
 var commandTime = Date.now();
 var resetTime = Date.now();
-var streamCheckTime = Date.now() - 10000;
 var messages = 0;
+
 
 
 
@@ -59,51 +57,17 @@ async function onMessageHandler (target, context, msg, self) {
         client.say(target, "I am a bot and can not whisper you back");
     }
 
-    // Remove whitespace from chat message
     const commandName = msg.trim().toLowerCase().replace("@", "").split(" ");
     const username = context.username;
 
     //If the command is !chain, make a chain and reset the cooldowns for auto print and commands
     if (commandName[0] == "!chain" && Date.now() - commandTime > commandCd) {
-        //If the command is of the form !chain <user>, check if that user has talked in the channel
-        if (commandName.length > 1) {
-        var months;
-        try {
-            months = await stream.getMonths(commandName[1], channel);
-        } catch (err) {
-            months = [];
-        }
-            if (months.length > 0) {
-                const viewer = commandName[1];
-                var messages = await stream.getMessages(months, viewer);
-                var userChunks = {};
-                var userPatterns = {};
-                //Take all messages from the user mentioned and convert it to Markov data
-                for (var i = 0; i < messages.length; i++) {
-                    if (! inBlacklist(messages[i])) {
-                        let data = MarkovChain.chunkText(messages[i], userPatterns, userChunks);
-                        userPatterns = data[0];
-                        userChunks = data[1];
-                    }
-                }
-
-                if (Object.keys(userPatterns).length > 10) {
-                    client.say(target, `${viewer}: ${MarkovChain.makeChain(userPatterns, userChunks)}`);
-                } else {
-                    client.say(target, MarkovChain.makeChain(textData, allChunks));
-                }
-            }
-        } else {
             client.say(target, MarkovChain.makeChain(textData, allChunks));
-        }
-
-        commandTime = Date.now();
-
-    } else if (commandName[0] == "!markovbot" && Date.now()) {
+            commandTime = Date.now();
+    } else if (commandName[0] == "!markovbot" && Date.now() - commandTime > commandCd) {
         client.say(target, `${context.username} Markov Chain Bot is a bot created by Buksss
             that imitates chat using a system called a Markov Chain. You can activate it
-            by typing !chain. You can also imitate a specific user by typing !chain <username>,
-            but this can take up to 20 seconds`);
+            by typing !chain, or by waiting for it to say something on its own`);
             commandTime = Date.now();
 
     } else {
@@ -115,7 +79,8 @@ async function onMessageHandler (target, context, msg, self) {
         let newData = MarkovChain.chunkText(msg, textData, allChunks);
         textData = newData[0];
         allChunks = newData[1];
-        if (messages >= 100) {
+        //If the cooldown for printing every 5 minutes is over, make a chain automatically
+        if (messages > 150) {
             client.say(target, MarkovChain.makeChain(textData, allChunks));
             messages = 0;
         }
